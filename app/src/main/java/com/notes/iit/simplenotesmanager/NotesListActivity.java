65ee6700551d16a14db44a606abbf568741efdbe
urmee91 <59314033+urmee91@users.noreply.github.com>
2019-12-30 10:37:18 -0800

    designed changed

---
+++ app/src/main/java/com/notes/iit/simplenotesmanager/NotesListActivity.java
@@ -0,0 +1,39 @@
+package com.notes.iit.simplenotesmanager;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+public class NotesListActivity extends AppCompatActivity {
+    FloatingActionButton noteEditOpenButton;
+    ListView listView;
+    SqliteHelper sqliteHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_list);
+        initalizeViews();
+        noteEditOpenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(NotesListActivity.this, ListEditActivity.class);
+                startActivity(intent);
+            }
+        });
+        sqliteHelper=new SqliteHelper(this);
+        Cursor cursor= sqliteHelper.retriveAllNotesCursor();
+        CursorAdapter cursorAdapter=new NotesListAdapter(this,cursor);
+        listView.setAdapter(cursorAdapter);
+    }
+
+    private void initalizeViews() {
+		//something
+        noteEditOpenButton=(FloatingActionButton)findViewById(R.id.fab);
+        listView=(ListView)findViewById(R.id.list);
+    }
+}
